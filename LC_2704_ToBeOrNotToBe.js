a3537e9acf9fa2818fd109398d41826baa1c2e78
vivek verma <verma022003@gmail.com>
2026-06-15 22:47:09 +0530

    2704

---
+++ LC_2704_ToBeOrNotToBe.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    function  toBe(v){
+        if(val===v){
+            return true;
+        }
+        throw new Error("Not Equal")
+    }
+    function notToBe(v){
+        if(val!==v){
+            return true;
+        }
+    throw new Error("Equal")
+    }
+
+    return {toBe,notToBe}
+    
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
